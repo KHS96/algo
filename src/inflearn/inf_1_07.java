2a49e573c93e403ae1b77a38df99b82bee57e78d
KHS96 <7heesu7@hanmail.net>
2023-04-01 12:06:22 +0900

    회문 문자열

--- src/inflearn/inf_1_07.java
+++ src/inflearn/inf_1_07.java
@@ -6,24 +6,12 @@ import java.io.InputStreamReader;
 
 public class inf_1_07 {
     static String solution(String str){
-        String answer = new String();
-        str = str.toUpperCase();
-        int lt = 0;
-        int rt = str.length()-1;
-        char []ch = str.toCharArray();
+        String answer = "NO" ;
+//        str = str.toUpperCase();
+        String temp = new StringBuilder(str).reverse().toString();
+        if(str.equalsIgnoreCase(temp)) answer = "YES";
 
-        while(lt<rt){
-            char temp = ch[lt];
-            ch[lt] = ch[rt];
-            ch[rt] = temp;
-            lt++;
-            rt--;
-        }
-        if(str.equals(String.valueOf(ch).toString()))
-            return "YES";
-        else{
-            return "NO";
-        }
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
