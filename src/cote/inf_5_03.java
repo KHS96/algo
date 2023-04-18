0f718f462613de747980c61908a099e0aef214dc
KHS96 <7heesu7@hanmail.net>
2023-04-18 10:15:11 +0900

    후위식 연산

--- src/cote/inf_5_03.java
+++ src/cote/inf_5_03.java
@@ -7,46 +7,43 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class inf_5_03 {
-    static int solution(int[][] board, int[] moves) {
+    static int solution(String s) {
         int ans = 0;
         Stack<Integer> stk = new Stack<>();
-        for (int i = 0; i < moves.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                int temp = board[j][moves[i] - 1];
-                if (temp != 0) {
-                    if (!stk.isEmpty() && stk.peek() == temp) {
-                        stk.pop();
-                        ans += 2;
-                    }else{
-                        stk.push(temp);
-                    }
-                    board[j][moves[i] - 1] = 0;
-                    break;
+        for(char x : s.toCharArray()){
+            if(Character.isDigit(x)){
+                stk.push(x-'0');
+            }else{
+                int b = stk.pop();
+                int f = stk.pop();
+                switch (x){
+                    case '-':
+                        stk.push(f-b);
+                        break;
+                    case '+':
+                        stk.push(f+b);
+                        break;
+                    case '*':
+                        stk.push(f*b);
+                        break;
+                    case '/':
+                        stk.push(f/b);
+                        break;
+                    default:
+                        break;
                 }
             }
         }
+        ans = stk.pop();
         return ans;
 
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int board[][] = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int m = Integer.parseInt(br.readLine());
-        int moves[] = new int[m];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            moves[i] = Integer.parseInt(st.nextToken());
-        }
+        String s = br.readLine();
 
-        System.out.println(solution(board, moves));
+        System.out.println(solution(s));
 
 
     }
