6b2a9e4fcc466a54eabf26347411858a2bc6e7ab
KHS96 <7heesu7@hanmail.net>
2023-04-24 13:47:10 +0900

    ignore

--- src/note/note1.java
+++ src/note/note1.java
@@ -1,48 +1,36 @@
 package note;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class note1 {
-    public static void main(String[] args) {
-        String[] kor = {"AAA", "BCD", "AAAAA", "ZY"};
-        String[] usa = {"AB", "AA", "TTTT"};
-        String[] incs = {"AB BCD AA TTTT AAAAA", "BCD AAA", "AB AAA TTTT BCD", "AA AAAAA AB", "AAA AB BCD"};
+    public static void main(String[] args) throws IOException {
 
-        System.out.println(solution(kor, usa, incs));
-        List<String> korList = new ArrayList<>(Arrays.asList(kor));
-        List<String> usaList = new ArrayList<>(Arrays.asList(usa));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [] arr = new int[n+1];
+        int [][]d = new int[n+1][2];
 
-        int cnt = 0 ;
-
-        for(int i=0;i< incs.length;i++){
-            String[] temp = incs[i].split(" ");
-
-            boolean chk1 = false;
-            boolean chk2 = false;
-            for(String x : temp){
-                if (korList.contains(x)){
-                    chk1=true;
-                }
-                if(usaList.contains(x)){
-                    chk2=true;
-                }
-            }
-            if(chk1 && chk2)
-                cnt++;
+        for(int i=1;i<=n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        System.out.println(cnt);
+        if(n==1){
+            System.out.println(arr[1]);
+            return ;
+        }
 
+        d[1][0] = arr[1];
+        d[1][1] = 0;
+        d[2][0] = arr[2];
+        d[2][1] = arr[1]+arr[2];
+
+        for(int i=3;i<=n;i++){
+            d[i][0] = Math.max(d[i-2][1],d[i-2][0])+arr[i];
+            d[i][1] = d[i-1][0]+arr[i];
+        }
+        System.out.println(Math.max(d[n][0],d[n][1]));
     }
 
-    public static int solution(String[] kor, String[] usa, String[] incs) {
-        List<String> korList = new ArrayList<>(Arrays.asList(kor));
-        List<String> usaList = new ArrayList<>(Arrays.asList(usa));
-
-        int max = -1;
-        for (int i = 0; i < incs.length; i++) {
-            String[] temp = incs[i].split(" ");
-            List<String> incsList = new ArrayList<>(Arrays.asList(incs));
-        }
-        return max;
-    }
 }
