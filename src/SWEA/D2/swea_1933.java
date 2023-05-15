aeda366f7b7b182e292c3e7e4a7dcedbdac0be2d
KHS96 <7heesu7@hanmail.net>
2023-05-15 10:57:31 +0900

    swea 1933

---
+++ src/SWEA/D2/swea_1933.java
@@ -0,0 +1,17 @@
+package SWEA.D2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class swea_1933 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0)
+                System.out.print(i+" ");
+        }
+    }
+}
