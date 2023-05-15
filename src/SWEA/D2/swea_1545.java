0f0b87d56ec9b2fb118380cd60a4df2fbb452e6c
KHS96 <7heesu7@hanmail.net>
2023-05-15 11:10:05 +0900

    swea 1545

---
+++ src/SWEA/D2/swea_1545.java
@@ -0,0 +1,16 @@
+package SWEA.D2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class swea_1545 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
