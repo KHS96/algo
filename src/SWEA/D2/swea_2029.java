cbe7630adc8c7a6350f9de5d66928037fc266082
KHS96 <7heesu7@hanmail.net>
2023-05-15 10:44:44 +0900

    swea 2029

--- src/SWEA/D2/swea_2029.java
+++ src/SWEA/D2/swea_2029.java
@@ -1,2 +1,23 @@
-package SWEA.D2;public class swea_2029 {
+package SWEA.D2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_2029 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println("#" + i + " " + a / b + " " + a % b);
+        }
+    }
 }
