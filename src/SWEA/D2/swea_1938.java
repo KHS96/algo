f0f5b042352e321dafc8a2cfb4c0ec9b850d6427
KHS96 <7heesu7@hanmail.net>
2023-05-15 10:56:15 +0900

    swea 1938

---
+++ src/SWEA/D2/swea_1938.java
@@ -0,0 +1,20 @@
+package SWEA.D2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_1938 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
